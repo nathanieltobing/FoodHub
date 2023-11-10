4c789711ccc7a1682fa1e8ed40d27f91b4f917e6
gearyr <gearyryandi@gmail.com>
2023-11-10 15:15:42 +0700

    hapus isi models\Book

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -7,14 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    use HasFactory;
 
-    public function publishers(){
-        return $this->hasOne(Publisher::class, 'id','publisher_id');
-    }
-
-    public function categories(){
-        return $this->belongsToMany(Category::class, 'book_categories', 'category_id', 'book_id');
-    }
-    // tes commit by geary
 }
