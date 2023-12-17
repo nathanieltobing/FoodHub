ea57a5cc2bba21514932e1a1afb2cadc2154f81c
Stevnatsan <stevnatsan@gmail.com>
2023-12-17 21:17:16 +0700

    form button

--- resources/views/editProduct.blade.php
+++ resources/views/editProduct.blade.php
@@ -7,6 +7,7 @@
         <div class="col-lg-12 col-xl-11">
           <div class="card text-black mb-5" style="border-radius: 25px;">
             <div class="card-body p-md-5">
+                <a href="/productList"> <i class="fa-solid fa-arrow-left fa-xl me-3 fa-fw"></i></a>
               <div class="row justify-content-center">
                 <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4" style="line-height :1.66; font-weight: 700; color:#222; font-family: Poppins;">FH</p>
 
@@ -27,66 +28,65 @@
                     {{-- <div class="d-flex flex-row align-items-center mb-4">
                       <img src="{{url($product->product_picture)}}" class="card-img-top" alt="..." style="height:500px">
                       </div>
-                    
+
                     </div> --}}
                     <div class="d-flex flex-row align-items-center mb-4">
-                      <i class="fas fa-user fa-lg me-3 fa-fw" style="margin-bottom: 30px"></i>
+                      <i class="fa-solid fa-pen-to-square fa-lg me-3 fa-fw" style="margin-bottom: 30px"></i>
                       <div class="form-outline flex-fill mb-0">
-                        <input type="text" id="form3Example1c" name="name" class="form-control" value="{{$product->name}}">
-                        <label class="form-label" for="form3Example1c">Product Name</label>
+                        <input type="text" id="form3Example1c" name="name" class="form-control" style="border: 1px solid;font-size:16 px;font-weight:500" value="{{$product->name}}">
+                        <label class="form-label" for="form3Example1c" style="font-size:16 px;font-weight:700">Product Name</label>
                       </div>
 
-                      
-                    
+
+
                     </div>
 
                     <div class="d-flex flex-row align-items-center mb-4">
-                      <i class="fas fa-envelope fa-lg me-3 fa-fw" style="margin-bottom: 30px"></i>
+                      <i class="fa-solid fa-square-poll-vertical fa-lg me-3 fa-fw" style="margin-bottom: 30px"></i>
                       <div class="form-outline flex-fill mb-0">
-                        <input type="number" id="form3Example3c" name="quantity" class="form-control" value={{$product->stock}}>
-                        <label class="form-label" for="form3Example3c">Quantity</label>
+                        <input type="number" id="form3Example3c" name="quantity" class="form-control" style="border: 1px solid;font-size:16 px;font-weight:500"" value={{$product->stock}}>
+                        <label class="form-label" for="form3Example3c" style="font-size:16 px;font-weight:700">Quantity</label>
                       </div>
                     </div>
-                    
+
                     <input type="hidden" name="role" value="CUSTOMER">
                     <div class="d-flex flex-row align-items-center mb-4">
-                      <i class="fas fa-lock fa-lg me-3 fa-fw" style="margin-bottom: 30px"></i>
+                      <i class="fa-solid fa-tags fa-lg me-3 fa-fw" style="margin-bottom: 30px"></i>
                       <div class="form-outline flex-fill mb-0">
-                        <input type="text" id="form3Example4c" name="price" class="form-control" value={{$product->price}}>
-                        <label class="form-label" for="form3Example4c">Price</label>
+                        <input type="text" id="form3Example4c" name="price" class="form-control" style="border: 1px solid;font-size:16 px;font-weight:500"" value={{$product->price}}>
+                        <label class="form-label" for="form3Example4c" style="font-size:16 px;font-weight:700">Price</label>
                       </div>
-                    
-                    </div>  
+
+                    </div>
 
                     <div class="d-flex flex-row align-items-center mb-4">
-                      <i class="fas fa-envelope fa-lg me-3 fa-fw" style="margin-bottom: 30px;"></i>
+                      <i class="fa-solid fa-list fa-lg me-3 fa-fw" style="margin-bottom: 30px;"></i>
                       <div class="form-outline flex-fill mb-0">
-                        <select class="select" name="category" id="format">                         
+                        <select class="select" name="category" id="format">
                           <option value="Main Course" {{strcmp($product->categories->name,"Main Course") == 0 ? 'selected' : ''}}>Main Course</option>
                           <option value="Appetizer" {{strcmp($product->categories->name,"Appetizer") == 0 ? 'selected' : ''}}>Appetizer</option>
-                          <option value="Desserts" {{strcmp($product->categories->name,"Desserts") == 0 ? 'selected' : ''}}>Desserts</option>                   
+                          <option value="Desserts" {{strcmp($product->categories->name,"Desserts") == 0 ? 'selected' : ''}}>Desserts</option>
                         </select>
                         <label class="form-label" style="display: flex;font-size:16 px;font-weight:700" for="form3Example3c">Category</label>
                       </div>
                     </div>
 
                     <div class="d-flex flex-row align-items-center mb-4">
-                      <i class="fas fa-user fa-lg me-3 fa-fw" style="margin-bottom: 30px;"></i>
-                      <div class="form-outline flex-fill mb-0">                 
-                        <textarea rows="3" cols="22" name="desc" placeholder="" class="form-control">{{$product->description}}</textarea>
-                        <label class="form-label" for="form3Example1c">Description</label>
+                      <i class="fa-solid fa-square-pen fa-lg me-3 fa-fw" style="margin-bottom: 30px;"></i>
+                      <div class="form-outline flex-fill mb-0">
+                        <textarea rows="3" cols="22" name="desc" placeholder="" class="form-control" style="border: 1px solid ;font-size:16 px;font-weight:500"">{{$product->description}}</textarea>
+                        <label class="form-label" for="form3Example1c" style="font-size:16 px;font-weight:700">Description</label>
                       </div>
                     </div>
                     <div class="d-flex flex-row align-items-center mb-4">
-                      <i class="fas fa-user fa-lg me-3 fa-fw" style="margin-bottom: 30px;"></i>
-                        <div class="form-outline flex-fill mb-0">
-                            <input class="form-control" name="dp" type="file" id="formFile">
-                          <label class="form-label" for="form3Example1c">Upload your product's picture</label>
+                      <i class="fas fa-image fa-lg me-3 fa-fw" style="margin-bottom: 30px;"></i>
+                        <div class="form-outline flex-fill mb-0" style="font-size:16 px;font-weight:700">
+                            <input class="form-control" name="dp" type="file" id="formFile"  style="border: 1px solid;font-size:16 px;font-weight:500">
+                          <label class="form-label" for="form3Example1c" style="font-size:16 px;font-weight:700">Upload your product's picture</label>
                         </div>
                     </div>
                     <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                      <button type="submit" class="btn btn-primary btn-lg" style="line-height :1.66; font-weight: 400; font-family: Poppins;">Edit Product</button>
-                    </div>
+                        <button type="submit" class="btn btn-primary btn-lg payment-form-submit-button" style="line-height :1.66; font-weight: 400; font-family: Poppins;width: 35%">Edit Product</button>                    </div>
 
                     <div class="row text-danger">
                       @if(session()->has('error'))
@@ -106,7 +106,7 @@
                 </div>
                 {{-- <div class="col-md-2 col-lg-2 col-xl-7 d-flex flex-column align-items-center order-1 order-lg-2" style="margin-top: -5px">
                     <div class="d-flex flex-row align-items-center mb-4 text-center h1 fw-bold">
-                       
+
                     </div>
                 </div> --}}
               </div>
